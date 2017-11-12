c5c6d6ffbf700b94dfacd51079f03c3f1a06ce15
Viktor Rocha <viktoorrocha@gmail.com>
2017-11-12 00:55:48 -0400

    Up

---
+++ app/src/main/java/helloandroid/br/com/deneb/DebugActivity.kt
@@ -0,0 +1,11 @@
+package helloandroid.br.com.deneb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class DebugActivity : AppCompatActivity(){
+    private val TAG = "Placas"
+    
+}
+
